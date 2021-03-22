478888009581fcc159eedcfd926db2f75ab3f0c8
Holmfred <fredrik1304@live.no>
2021-03-22 18:08:09 +0100

    oppsett

---
+++ js/script.js
@@ -0,0 +1,72 @@
+//Question 1
+
+const instrument = "guitar";
+console.log(instrument)
+
+//Question 2
+
+const button = document.querySelector("button.login");
+
+//Question 3
+
+const instruments = [
+    {
+        type: "guitar",
+        colour: "red",
+    },
+    {
+        type: "piano",
+        colour: "black",
+    },
+];
+
+//Question 4
+
+var name = "Percival";
+var age = 13;
+
+//Question 5
+
+var total = 0;
+total = total + 10;
+
+//Question 6
+
+for (var i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+//Question 7
+
+const firstName = "Florence";
+
+const introduction = "Hello, my name is " + firstName + ".";
+
+console.log(introduction);
+
+//Question 8
+
+const title = "Big Technical Event";
+
+const whatToHave = "good time";
+
+const welcome = "Welcome! \n\n" +
+                "This " + title + " is starting today.\n\n" +
+                "Have a " + whatToHave + "!";
+
+console.log(welcome);
+
+//Question 9
+
+const car = {
+    "paint-colour": "red",
+    "number of wheels": 3
+};
+
+//Question 10
+
+const product = {
+    name: "Chicken Lips",
+    price: 35,
+    expired: false
+}
