c51bae7a0ecabf0ab8e7702b72cb5a3c06531dae
Holmfred <fredrik1304@live.no>
2021-03-22 21:25:29 +0100

    Done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 //Question 1
 
 const instrument = "guitar";
-console.log(instrument)
+console.log(instrument);
 
 //Question 2
 
 const button = document.querySelector("button.login");
+console.dir(button);
 
 //Question 3
 
@@ -20,14 +21,22 @@ const instruments = [
     },
 ];
 
+console.table(instrument);
+
 //Question 4
 
 var name = "Percival";
 var age = 13;
 
+const name = "Percival";
+const age = 13;
+
 //Question 5
 
 var total = 0;
+total = total + 10;
+
+let total = 0;
 total = total + 10;
 
 //Question 6
@@ -36,11 +45,21 @@ for (var i = 0; i <= 5; i++) {
     console.log(i);
 }
 
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
 //Question 7
 
 const firstName = "Florence";
 
 const introduction = "Hello, my name is " + firstName + ".";
+
+console.log(introduction);
+
+const firstName = "Florence";
+
+const introduction = `Hello, my name is ${firstName}.`;
 
 console.log(introduction);
 
@@ -56,12 +75,25 @@ const welcome = "Welcome! \n\n" +
 
 console.log(welcome);
 
+const title = "Big Technical Event";
+
+const whatToHave = "good time";
+
+const welcome = `Welcome!
+This ${title} is starting today.
+Have a ${whatToHave}!`;
+
+console.log(welcome);
+
 //Question 9
 
 const car = {
     "paint-colour": "red",
     "number of wheels": 3
 };
+
+console.log(car["paint-colour"]);
+console.log(car["number of wheels"]);
 
 //Question 10
 
@@ -70,3 +102,14 @@ const product = {
     price: 35,
     expired: false
 }
+
+const product = {
+    name: "Chicken Lips",
+    price: 35,
+    expired: true,
+    getExpired: function() {
+        console.log(this.expired);
+    }
+}
+
+product.getExpired();
